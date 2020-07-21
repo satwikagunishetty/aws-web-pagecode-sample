a89aeb2ee5797c8e45e8ef3544dedd81ff12df01
satwikagunishetty <60072014+satwikagunishetty@users.noreply.github.com>
2020-07-21 19:30:47 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>HTML Image</h2>
+<img src="moon.jpg"" alt="moon" width="500" height="333">
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
