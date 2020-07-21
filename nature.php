9eb11513a8ab380cf304fd272c995876583adf3d
satwikagunishetty <60072014+satwikagunishetty@users.noreply.github.com>
2020-07-21 18:11:02 +0530

    Update nature.php

--- nature.php
+++ nature.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <h2>HTML Image</h2>
-<img src="moon.jpg"" alt="Trulli" width="500" height="333">
+<img src="moon.jpg"" alt="moon" width="500" height="333">
 <?php
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
